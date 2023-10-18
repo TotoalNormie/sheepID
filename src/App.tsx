eb0380c3075f5d53769d0c4817829fef11757414
Total Normie <109313344+TotoalNormie@users.noreply.github.com>
2023-10-18 20:11:56 +0300

    added features

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import SheepTable from './components/SheepTable.tsx';
+import SheepRow from './components/SheepRow.tsx';
 
 function App() {
   const sheep = [1232, 5122, 241, 5342, 1242, 1234];
-
+  const sheepList = sheep.map((num: number) => { return <SheepRow>{num}</SheepRow>});
   return (
     <>
       <header></header>
       <main>
         <h1>Sheep ID</h1>
-        <SheepTable sheep={sheep}/>
+        <ol>
+          {sheepList}
+        </ol>
       </main>
       <footer></footer>
     </>
---
+++ src/components/SheepRow.tsx
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+interface Props {
+    children: number;
+}
+
+const SheepRow = ({ children }: Props) => {
+    return (
+        <li>
+            {children}
+            <button>
+                <FontAwesomeIcon icon={faPen} />
+            </button>
+            <button>
+            <FontAwesomeIcon icon={faTrash} />
+            </button>
+        </li>
+    );
+}
+
+export default SheepRow;
